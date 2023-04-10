a47e4acd3d2d10bf9590c7028b6818f220f48136
mariobes <128904643+mariobes@users.noreply.github.com>
2023-04-10 19:17:32 +0200

    Añadir los casos con las letras excepcionales
    Escribimos más test, entre ellos casos excepcionales como el número 4 o 9, y los añadimos a la lista de las letras romanas

--- src/test/java/NumerosRomanosTest.java
+++ src/test/java/NumerosRomanosTest.java
@@ -18,37 +18,61 @@ public class NumerosRomanosTest {
     }
 
     @Test
-    public void _5_Devuelve_V() {
-       assertEquals("V", numerosRomanos.play(5));
+    public void _2_Devuelve_II() {
+        assertEquals("II", numerosRomanos.play(2));
     }
 
     @Test
-    public void _10_Devuelve_X() {
-        assertEquals("X", numerosRomanos.play(10));
+    public void _3_Devuelve_III() {
+        assertEquals("III", numerosRomanos.play(3));
     }
 
     @Test
-    public void _50_Devuelve_L() {
-        NumerosRomanos numerosRomanos = new NumerosRomanos();
-        assertEquals("L", numerosRomanos.play(50));
+    public void _4_Devuelve_IV() {
+        assertEquals("IV", numerosRomanos.play(4));
     }
 
     @Test
-    public void _100_Devuelve_C() {
-        NumerosRomanos numerosRomanos = new NumerosRomanos();
-        assertEquals("C", numerosRomanos.play(100));
-    }
+    public void _5_Devuelve_V() { assertEquals("V", numerosRomanos.play(5)); }
 
     @Test
-    public void _500_Devuelve_D() {
-        NumerosRomanos numerosRomanos = new NumerosRomanos();
-        assertEquals("D", numerosRomanos.play(500));
-    }
+    public void _9_Devuelve_IX() { assertEquals("IX", numerosRomanos.play(9)); }
 
     @Test
-    public void _1000_Devuelve_M() {
-        NumerosRomanos numerosRomanos = new NumerosRomanos();
-        assertEquals("M", numerosRomanos.play(1000));
-    }
+    public void _10_Devuelve_X() { assertEquals("X", numerosRomanos.play(10)); }
+
+    @Test
+    public void _20_Devuelve_XX() { assertEquals("XX", numerosRomanos.play(20)); }
+
+    @Test
+    public void _30_Devuelve_XXX() { assertEquals("XXX", numerosRomanos.play(30)); }
+
+    @Test
+    public void _40_Devuelve_XL() { assertEquals("XL", numerosRomanos.play(40)); }
+
+    @Test
+    public void _50_Devuelve_L() { assertEquals("L", numerosRomanos.play(50)); }
+
+    @Test
+    public void _100_Devuelve_C() { assertEquals("C", numerosRomanos.play(100)); }
+
+    @Test
+    public void _400_Devuelve_CD() { assertEquals("CD", numerosRomanos.play(400)); }
+
+    @Test
+    public void _500_Devuelve_D() { assertEquals("D", numerosRomanos.play(500)); }
+
+    @Test
+    public void _900_Devuelve_CM() { assertEquals("CM", numerosRomanos.play(900)); }
+
+    @Test
+    public void _1000_Devuelve_M() { assertEquals("M", numerosRomanos.play(1000)); }
+
+    @Test
+    public void _1949_Devuelve_MCMXLIX() { assertEquals("MCMXLIX", numerosRomanos.play(1949 )); }
+
+    @Test
+    public void _2018_Devuelve_MMXVIII() { assertEquals("MMXVIII", numerosRomanos.play(2018 )); }
+
 
 }
