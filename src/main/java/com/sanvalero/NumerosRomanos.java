9f650597146b1894b6f3278db298a8a6f897ebdc
mariobes <128904643+mariobes@users.noreply.github.com>
2023-04-10 19:08:15 +0200

    Añadir lista con los numeros y su valor
    Creamos un método donde establecemos cada letra romana y su valor correspondiente

--- src/main/java/com/sanvalero/NumerosRomanos.java
+++ src/main/java/com/sanvalero/NumerosRomanos.java
@@ -1,6 +1,23 @@
 package com.sanvalero;
 
 public class NumerosRomanos {
+
+    enum ValorLetrasRomanas {
+        I(1),
+        V(5),
+        X(10),
+        L(50),
+        C(100),
+        D(500),
+        M(1000);
+
+        int valor;
+
+        ValorLetrasRomanas(int valor) {
+            this.valor = valor;
+
+        }
+    }
     public String play(int numero) {
 
         StringBuilder resultado = new StringBuilder();
